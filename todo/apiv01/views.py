4aaf59bf72154442364f271ba5e22c04b797d005
alishermutalov <96579968+alishermutalov@users.noreply.github.com>
2024-10-10 16:41:24 +0500

    Fixed bug of registration

--- todo/apiv01/views.py
+++ todo/apiv01/views.py
@@ -8,6 +8,7 @@ from .serializers import LoginSerializer, LogoutSerializer, RegisterSerializer
 
 
 class RegisterView(APIView):
+    permission_classes = [AllowAny,]
     
     def post(self, request):
         serializer = RegisterSerializer(data=request.data)
